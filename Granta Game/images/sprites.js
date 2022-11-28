0e0a21606ef2eec1e804effea98bbdf224cd4b31
Данил <119154169+DanyaTravka@users.noreply.github.com>
2022-11-29 00:06:39 +0500

    Delete Granta Game directory

--- Granta Game/images/sprites.js
+++
@@ -1,8 +0,0 @@
-var SPRITES = {
-  PLAYER_UPHILL_LEFT:     { x: 1383, y:  961, w:   80, h:   45 },
-  PLAYER_UPHILL_STRAIGHT: { x: 1295, y: 1018, w:   80, h:   45 },
-  PLAYER_UPHILL_RIGHT:    { x: 1385, y: 1018, w:   80, h:   45 },
-  PLAYER_LEFT:            { x:  995, y:  480, w:   80, h:   41 },
-  PLAYER_STRAIGHT:        { x: 1085, y:  480, w:   80, h:   41 },
-  PLAYER_RIGHT:           { x:  995, y:  531, w:   80, h:   41 }
-};
